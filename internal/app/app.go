11a1d5d8629fc8da000f9389bf07fdb7349841a1
underthetreee <153267269+underthetreee@users.noreply.github.com>
2024-01-22 09:57:51 +0200

    Implement graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,14 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -13,19 +19,42 @@ import (
 func Run() error {
 	cfg, err := config.Init()
 	if err != nil {
-		return fmt.Errorf("failed to init config: %w", err)
+		return fmt.Errorf("init config: %w", err)
 	}
 
 	db, err := sqlx.Connect("postgres", cfg.Postgres.URI)
 	if err != nil {
-		return err
+		return fmt.Errorf("connect postgres: %w", err)
 	}
 	defer db.Close()
 
 	srv := server.NewServer(cfg)
+
+	var (
+		quitch = make(chan os.Signal, 1)
+		errch  = make(chan error, 1)
+	)
+
+	go func() {
+		if err := srv.Run(); err != http.ErrServerClosed {
+			errch <- err
+		}
+	}()
 	log.Printf("server is listening on :%s", cfg.HTTP.Port)
-	if err := srv.Run(); err != nil {
-		return fmt.Errorf("failed to start http server: %w", err)
+
+	signal.Notify(quitch, syscall.SIGTERM, syscall.SIGINT)
+
+	select {
+	case <-quitch:
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		if err := srv.Stop(ctx); err != nil {
+			return fmt.Errorf("stop http server: %w", err)
+		}
+	case err := <-errch:
+		return fmt.Errorf("start http server: %w", err)
 	}
 	return nil
 }
